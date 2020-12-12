97d9ab156115e099f71eb65b73cdf1842c2e5f5e
vaibhavAccionLabs <vaibhav.katariya@accionlabs.com>
2020-12-12 20:00:39 +0530

    implement feature

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,8 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Container App</h1>
+      <h1>Container App !!!!!!</h1>
+      <hr />
       <MarketingApp />
     </div>
   );
